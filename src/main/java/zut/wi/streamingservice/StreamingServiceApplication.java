663ddaef1fe1b212690c431e5450137368d441fb
Vladimir Marianciuc <boosth14@gmail.com>
2024-05-09 23:35:05 +0200

    Updated application configuration

--- src/main/java/zut/wi/streamingservice/StreamingServiceApplication.java
+++ src/main/java/zut/wi/streamingservice/StreamingServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StreamingserviceApplication {
+public class StreamingServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StreamingserviceApplication.class, args);
+		SpringApplication.run(StreamingServiceApplication.class, args);
 	}
 
 }
